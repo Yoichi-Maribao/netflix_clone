5be83404ece8aaff391cda613ab4e0d7c5d4fb4c
Yoichi-Maribao <szk.m.yo1@gmail.com>
2022-05-26 20:26:58 +0900

    done Banner

---
+++ src/components/Banner.tsx
@@ -0,0 +1,64 @@
+import axios from '../api/axios';
+import { useEffect, useState } from 'react';
+import { requests } from '../api/request';
+import './Banner.scss';
+
+type MovieProps = {
+  title?: string;
+  name?: string;
+  original_name?: string;
+  backdrop_path?: string;
+  overview?: string;
+};
+
+export const Banner = () => {
+  const [movie, setMovie] = useState<MovieProps>({});
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.feachNetflixOriginals);
+      console.log(request.data.result);
+
+      // pick value randomly from the API
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+    }
+    fetchData();
+  }, []);
+  console.log(movie);
+
+  // afunction for truncate description
+  function truncate(str: any, n: number) {
+    // guard undefined
+    if (str !== undefined) {
+      return str.length > n ? str?.substr(0, n - 1) + '...' : str;
+    }
+  }
+
+  return (
+    <header
+      className="Banner"
+      style={{
+        backgroundSize: 'cover',
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+        backgroundPosition: 'center center',
+      }}
+    >
+      <div className="Banner-contents">
+        <h1 className="Banner-title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="Banner-buttons">
+          <button className="Banner-button">Play</button>
+          <button className="Banner-button">My List</button>
+        </div>
+
+        <h1 className="Banner-description">{truncate(movie?.overview, 150)}</h1>
+      </div>
+
+      <div className="Banner-fadeBottom"></div>
+    </header>
+  );
+};
---
+++ src/components/Row.tsx
@@ -0,0 +1,55 @@
+import axios from '../api/axios';
+import { useEffect, useState } from 'react';
+import './Row.scss';
+
+const base_url = 'https://image.tmdb.org/t/p/original';
+
+type Props = {
+  title: string;
+  fetchUrl: string;
+  isLargeRow?: boolean;
+};
+
+type Movie = {
+  id: string;
+  name: string;
+  title: string;
+  original_name: string;
+  poster_path: string;
+  backdrop_path: string;
+};
+
+export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      console.log();
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  console.log(movies);
+
+  return (
+    <div className="Row">
+      <h2>{title}</h2>
+      <div className="Row-posters">
+        {/* ポスターコンテンツ */}
+        {movies.map((movie) => (
+          <img
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+            key={movie.id}
+            className={`Row-poster ${isLargeRow && 'Row-poster-lrarge'}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
